04eec47731ea7af55083c0540035380c9355f29f
Amy-oo1 <2965168884@qq.com>
2025-03-13 22:25:26 +0800

    add transform component

---
+++ function/include/frame/compoents/transform/Transform_Component.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include<cstddef>
+#include<vector>
+
+#include "transform/Affine_Transform.h"
+
+#include "meta/reflection/Reflection_Macro.h"
+
+#include "frame/compoents/Compoent.h"
+#include "frame/object/GObject.h"
+
+namespace NameSpace_Function::Namespace_Frame::NameSpace_Components::NameSpace_Transform {
+
+	using std::vector;
+
+	using NameSpace_Core::NameSpace_Transform::Affine_Transform;
+
+	using NameSpace_GObject::GObject;
+
+	REFLECTION_HEADER(Transform_Component);
+	REFLECTION_CLASS(Transform_Component, All) final :public Component
+	{
+		REFLECTION_BODY(Transform_Component)
+	public:
+		Transform_Component(void) = default;
+
+		virtual ~Transform_Component(void) override = default;
+
+	public:
+		virtual void Post_Load_Resource(weak_ptr<GObject> Parent_Object) override;
+		virtual void Tick(float Delta_Time) override;
+
+		const Affine_Transform& Get_Current_Transform(void)const;
+
+		void Set_New_Transform(const Affine_Transform& Transform);
+
+	protected:
+		Affine_Transform m_Transform{ Affine_Transform::IDENTITY };
+
+		vector<Affine_Transform> m_Transform_Buffer{ Affine_Transform::IDENTITY ,Affine_Transform::IDENTITY };
+
+		size_t m_Currrent_Buffer_Index{ 0 };
+		size_t m_Next_Buffer_Index{ 1 };
+
+	};
+
+}// namespace NameSpace_Function::Namespace_Frame::NameSpace_Com
---
+++ function/src/frame/compoents/transform/Transform_Component.cpp
@@ -0,0 +1,36 @@
+#include "frame/compoents/transform/Transform_Component.h"
+
+#include<utility>
+
+namespace NameSpace_Function::Namespace_Frame::NameSpace_Components::NameSpace_Transform {
+
+	void Transform_Component::Post_Load_Resource(weak_ptr<GObject> Parent_Object) {
+		this->Component::m_Parent_GObject = Parent_Object;
+
+		this->m_Transform_Buffer[this->m_Currrent_Buffer_Index] = m_Transform;
+		this->m_Transform_Buffer[this->m_Next_Buffer_Index] = m_Transform;
+
+		this->Component::m_Is_Dirty = true;
+	}
+
+	void Transform_Component::Tick(float Delta_Time) {
+		std::swap(this->m_Currrent_Buffer_Index, this->m_Next_Buffer_Index);
+
+		if (this->Component::m_Is_Dirty) {
+			//TODO : Implement the logic to update the transform of the parent object
+		}
+
+		//TODO : g_is_editor_mode is a global variable that is set to true when the editor is running
+	}
+
+	const Affine_Transform& Transform_Component::Get_Current_Transform(void) const {
+		return this->m_Transform_Buffer[this->m_Currrent_Buffer_Index];
+	}
+
+	void Transform_Component::Set_New_Transform(const Affine_Transform& Transform) {
+		this->m_Transform_Buffer[this->m_Next_Buffer_Index] = Transform;
+
+		this->Component::m_Is_Dirty = true;
+	}
+
+}// namespace NameSpace_Function::Namespace_Frame::NameSpace_Com
