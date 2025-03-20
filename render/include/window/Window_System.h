4366471784efd425be2273a82fce36158aeaa511
Amy-oo1 <2965168884@qq.com>
2025-03-20 14:52:57 +0800

    finish window

---
+++ render/include/window/Window_System.h
@@ -0,0 +1,135 @@
+#pragma once
+
+#include "../../macro.h"
+
+#define GLFW_INCLUDE_VULKAN
+#include "GLFW/glfw3.h"
+
+#include<cstdint>
+#include<limits>
+#include<tuple>
+#include<vector>
+#include<string>
+#include<functional>
+#include<memory>
+#include<exception>
+
+namespace NameSpace_Render::NameSpace_Window {
+
+	using std::tuple;
+	using std::vector;
+	using std::string;
+	using std::function;
+	using std::unique_ptr;
+
+	struct Window_System_Initialization_Info final {
+		uint32_t Width{ 1280 };
+		uint32_t Height{ 720 };
+		bool Is_Full_Screen{ false };
+
+		const char* Titile{ "Amy_Engine" };
+	};
+
+	class Window_System final {
+	public:
+		using On_Reset_Func = std::function<void(void)>;
+		using On_Key_Func = std::function<void(int, int, int, int)>;
+		using On_Char_Func = std::function<void(unsigned int)>;
+		using On_CharMods_Func = std::function<void(unsigned int, int)>;
+		using On_Mouse_Button_Func = std::function<void(int, int, int)>;
+		using On_Cursor_Pos_Func = std::function<void(double, double)>;
+		using On_Cursor_Enter_Func = std::function<void(int)>;
+		using On_Scroll_Func = std::function<void(double, double)>;
+		using On_Drop_Func = std::function<void(int, const char**)>;
+
+	private:
+		Window_System(void) = delete;
+
+		Window_System(const Window_System&) = delete;
+		Window_System& operator=(const Window_System&) = delete;
+
+	public:
+		Window_System(const Window_System_Initialization_Info& init_info);
+
+		~Window_System() = default;
+
+	public:
+		void Poll_Window_Events(void);
+
+		bool Is_WIndow_Shoule_Close(void);
+		void ReSet_Window_Title(const string& Title);
+		const tuple<uint32_t, uint32_t> Get_Window_Size(void) const;
+
+		void Register_On_Reset_Func(const On_Reset_Func& Func);
+		void Register_On_Key_Func(const On_Key_Func& Func);
+		void Register_On_Char_Func(const On_Char_Func& Func);
+		void Register_On_CharMods_Func(const On_CharMods_Func& Func);
+		void Register_On_Mouse_Button_Func(const On_Mouse_Button_Func& Func);
+		void Register_On_Cursor_Pos_Func(const On_Cursor_Pos_Func& Func);
+		void Register_On_Cursor_Enter_Func(const On_Cursor_Enter_Func& Func);
+		void Register_On_Scroll_Func(const On_Scroll_Func& Func);
+		void Register_On_Drop_Func(const On_Drop_Func& Func);
+
+
+
+	private:
+		void On_Reset(void);
+		void On_Key(int Key, int Scancode, int Action, int Mods);
+		void On_Char(unsigned int CodePoint);
+		void On_CharMods(unsigned int CodePoint, int Mods);
+		void On_Mouse_Button(int Button, int Action, int Mods);
+		void On_Cursor_Pos(double X_Pos, double Y_Pos);
+		void On_Cursor_Enter(int Entered);
+		void On_Scroll(double X_Offset, double Y_Offset);
+		void On_Drop(int Count, const char** Paths);
+
+	private:
+		static void Initialize_GLWF(void);
+
+		static void Reset_Call_Back(GLFWwindow* Window);
+		static void Key_Call_Back(GLFWwindow* Window, int Key, int Scancode, int Action, int Mods);
+		static void Char_Call_Back(GLFWwindow* Window, unsigned int CodePoint);
+		static void CharMods_Call_Back(GLFWwindow* Window, unsigned int CodePoint, int Mods);
+		static void MouseButton_Call_Back(GLFWwindow* Window, int Button, int Action, int Mods);
+		static void CursorPos_Call_Back(GLFWwindow* Window, double X_Pos, double Y_Pos);
+		static void CursorEnter_Call_Back(GLFWwindow* Window, int Entered);
+		static void Scroll_Call_Back(GLFWwindow* Window, double X_Offset, double Y_Offset);
+		static void Drop_Call_Back(GLFWwindow* Window, int Count, const char** Paths);
+		static void Window_Resize_Call_Back(GLFWwindow* Window, int Width, int Height);
+		static void Window_Close_Call_Back(GLFWwindow* Window);
+
+	private:
+		static inline uint32_t s_Window_Count{ 0 };
+
+		static constexpr uint32_t s_Max_Window_Count{ std::numeric_limits<uint32_t>::max() };
+
+		static constexpr auto Delete_GLFW_Window = [](GLFWwindow* Window)
+			{
+				if (nullptr != Window) {
+					glfwDestroyWindow(Window);
+					if (0 == --s_Window_Count)
+						glfwTerminate();
+				}
+				else
+					LOG_ERROR("Window is nullptr");
+			};
+
+	private:
+		unique_ptr<GLFWwindow, decltype(Delete_GLFW_Window)> m_Window{ nullptr,Window_System::Delete_GLFW_Window };
+		uint32_t m_Width{ 0 };
+		uint32_t m_Height{ 0 };
+		bool m_Is_Full_Screen{ false };
+
+		vector<On_Reset_Func> m_Reset_Call_Backs{};
+		vector<On_Key_Func> m_Key_Call_Backs{};
+		vector<On_Char_Func> m_Char_Call_Backs{};
+		vector<On_CharMods_Func> m_CharMods_Call_Backs{};
+		vector<On_Mouse_Button_Func> m_Mouse_Button_Call_Backs{};
+		vector<On_Cursor_Pos_Func> m_Cursor_Pos_Call_Backs{};
+		vector<On_Cursor_Enter_Func> m_Cursor_Enter_Call_Backs{};
+		vector<On_Scroll_Func> m_Scroll_Call_Backs{};
+		vector<On_Drop_Func> m_Drop_Call_Backs{};
+
+	};
+
+} // namespace NameSpace_Render::NameSpace_Window
---
+++ render/src/window/Window_System.cpp
@@ -0,0 +1,235 @@
+#include "window/Window_System.h"
+
+#include<mutex>
+
+namespace NameSpace_Render::NameSpace_Window {
+
+	Window_System::Window_System(const Window_System_Initialization_Info& init_info) {
+		static std::once_flag Initialized_GLWF_Flag{};
+
+		std::call_once(Initialized_GLWF_Flag, Window_System::Initialize_GLWF);
+
+		this->m_Window.reset(glfwCreateWindow(init_info.Width, init_info.Height, init_info.Titile, nullptr, nullptr));
+		if (nullptr == this->m_Window)
+			LOG_ERROR("Failed to create window");
+
+		++Window_System::s_Window_Count;
+
+		this->m_Width = init_info.Width;
+		this->m_Height = init_info.Height;
+
+		glfwSetInputMode(this->m_Window.get(), GLFW_STICKY_KEYS, GLFW_FALSE);
+		glfwSetInputMode(this->m_Window.get(), GLFW_STICKY_MOUSE_BUTTONS, GLFW_FALSE);
+		glfwSetInputMode(this->m_Window.get(), GLFW_RAW_MOUSE_MOTION, GLFW_FALSE);
+
+		//Set The Callback
+		{
+			//TODO : 
+			glfwSetWindowUserPointer(this->m_Window.get(), this);
+			glfwSetKeyCallback(this->m_Window.get(), Window_System::Key_Call_Back);
+			glfwSetCharModsCallback(this->m_Window.get(), Window_System::CharMods_Call_Back);
+			glfwSetCharCallback(this->m_Window.get(), Window_System::Char_Call_Back);
+			glfwSetMouseButtonCallback(this->m_Window.get(), Window_System::MouseButton_Call_Back);
+			glfwSetCursorPosCallback(this->m_Window.get(), Window_System::CursorPos_Call_Back);
+			glfwSetCursorEnterCallback(this->m_Window.get(), Window_System::CursorEnter_Call_Back);
+			glfwSetScrollCallback(this->m_Window.get(), Window_System::Scroll_Call_Back);
+			glfwSetDropCallback(this->m_Window.get(), Window_System::Drop_Call_Back);
+			glfwSetWindowSizeCallback(this->m_Window.get(), Window_System::Window_Resize_Call_Back);
+			glfwSetWindowCloseCallback(this->m_Window.get(), Window_System::Window_Close_Call_Back);
+		}
+	}
+
+	void Window_System::Poll_Window_Events(void) {
+		glfwPollEvents();
+	}
+
+	bool Window_System::Is_WIndow_Shoule_Close(void) {
+		return glfwWindowShouldClose(this->m_Window.get());
+	}
+
+	void Window_System::ReSet_Window_Title(const string& Title) {
+		glfwSetWindowTitle(this->m_Window.get(), Title.c_str());
+	}
+
+	const tuple<uint32_t, uint32_t> Window_System::Get_Window_Size(void) const {
+		return { this->m_Width,this->m_Height };
+	}
+
+	void NameSpace_Window::Window_System::Register_On_Reset_Func(const On_Reset_Func& Func) {
+		this->m_Reset_Call_Backs.push_back(Func);
+	}
+
+	void Window_System::Register_On_Key_Func(const On_Key_Func& Func) {
+		this->m_Key_Call_Backs.push_back(Func);
+	}
+
+	void Window_System::Register_On_Char_Func(const On_Char_Func& Func) {
+		this->m_Char_Call_Backs.push_back(Func);
+	}
+
+	void Window_System::Register_On_CharMods_Func(const On_CharMods_Func& Func) {
+		this->m_CharMods_Call_Backs.push_back(Func);
+	}
+
+	void Window_System::Register_On_Mouse_Button_Func(const On_Mouse_Button_Func& Func) {
+		this->m_Mouse_Button_Call_Backs.push_back(Func);
+	}
+
+	void Window_System::Register_On_Cursor_Pos_Func(const On_Cursor_Pos_Func& Func) {
+		this->m_Cursor_Pos_Call_Backs.push_back(Func);
+	}
+
+	void Window_System::Register_On_Cursor_Enter_Func(const On_Cursor_Enter_Func& Func) {
+		this->m_Cursor_Enter_Call_Backs.push_back(Func);
+	}
+
+	void Window_System::Register_On_Scroll_Func(const On_Scroll_Func& Func) {
+		this->m_Scroll_Call_Backs.push_back(Func);
+	}
+
+	void Window_System::Register_On_Drop_Func(const On_Drop_Func& Func) {
+		this->m_Drop_Call_Backs.push_back(Func);
+	}
+
+	void NameSpace_Window::Window_System::On_Reset(void) {
+		for (const auto& Func : this->m_Reset_Call_Backs)
+			Func();
+	}
+
+	void Window_System::On_Key(int Key, int Scancode, int Action, int Mods) {
+		for (const auto& Func : this->m_Key_Call_Backs)
+			Func(Key, Scancode, Action, Mods);
+	}
+
+	void Window_System::On_Char(unsigned int CodePoint) {
+		for (const auto& Func : this->m_Char_Call_Backs)
+			Func(CodePoint);
+	}
+
+	void Window_System::On_CharMods(unsigned int CodePoint, int Mods) {
+		for (const auto& Func : this->m_CharMods_Call_Backs)
+			Func(CodePoint, Mods);
+	}
+
+	void Window_System::On_Mouse_Button(int Button, int Action, int Mods) {
+		for (const auto& Func : this->m_Mouse_Button_Call_Backs)
+			Func(Button, Action, Mods);
+	}
+
+	void Window_System::On_Cursor_Pos(double X_Pos, double Y_Pos) {
+		for (const auto& Func : this->m_Cursor_Pos_Call_Backs)
+			Func(X_Pos, Y_Pos);
+	}
+
+	void Window_System::On_Cursor_Enter(int Entered) {
+		for (const auto& Func : this->m_Cursor_Enter_Call_Backs)
+			Func(Entered);
+	}
+
+	void Window_System::On_Scroll(double X_Offset, double Y_Offset) {
+		for (const auto& Func : this->m_Scroll_Call_Backs)
+			Func(X_Offset, Y_Offset);
+	}
+
+	void Window_System::On_Drop(int Count, const char** Paths) {
+		for (const auto& Func : this->m_Drop_Call_Backs)
+			Func(Count, Paths);
+	}
+
+	void Window_System::Initialize_GLWF(void) {
+		if (GLFW_FALSE == glfwInit())
+			LOG_ERROR("Failed to initialize GLFW");
+
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+	}
+
+	void NameSpace_Window::Window_System::Reset_Call_Back(GLFWwindow* Window) {
+		const auto& App{ static_cast<Window_System*>(glfwGetWindowUserPointer(Window)) };
+		if (nullptr == App)
+			LOG_ERROR("App is nullptr");
+
+		App->On_Reset();
+	}
+
+	void Window_System::Key_Call_Back(GLFWwindow* Window, int Key, int Scancode, int Action, int Mods) {
+		const auto& App{ static_cast<Window_System*>(glfwGetWindowUserPointer(Window)) };
+		if (nullptr == App)
+			LOG_ERROR("App is nullptr");
+
+		App->On_Key(Key, Scancode, Action, Mods);
+	}
+
+	void Window_System::Char_Call_Back(GLFWwindow* Window, unsigned int CodePoint) {
+		const auto& App{ static_cast<Window_System*>(glfwGetWindowUserPointer(Window)) };
+		if (nullptr == App)
+			LOG_ERROR("App is nullptr");
+
+		App->On_Char(CodePoint);
+	}
+
+	void Window_System::CharMods_Call_Back(GLFWwindow* Window, unsigned int CodePoint, int Mods) {
+		const auto& App{ static_cast<Window_System*>(glfwGetWindowUserPointer(Window)) };
+		if (nullptr == App)
+			LOG_ERROR("App is nullptr");
+
+		App->On_CharMods(CodePoint, Mods);
+	}
+
+	void Window_System::MouseButton_Call_Back(GLFWwindow* Window, int Button, int Action, int Mods) {
+		const auto& App{ static_cast<Window_System*>(glfwGetWindowUserPointer(Window)) };
+		if (nullptr == App)
+			LOG_ERROR("App is nullptr");
+
+		App->On_Mouse_Button(Button, Action, Mods);
+	}
+
+	void Window_System::CursorPos_Call_Back(GLFWwindow* Window, double X_Pos, double Y_Pos) {
+		const auto& App{ static_cast<Window_System*>(glfwGetWindowUserPointer(Window)) };
+		if (nullptr == App)
+			LOG_ERROR("App is nullptr");
+
+		App->On_Cursor_Pos(X_Pos, Y_Pos);
+	}
+
+	void Window_System::CursorEnter_Call_Back(GLFWwindow* Window, int Entered) {
+		const auto& App{ static_cast<Window_System*>(glfwGetWindowUserPointer(Window)) };
+		if (nullptr == App)
+			LOG_ERROR("App is nullptr");
+
+		App->On_Cursor_Enter(Entered);
+	}
+
+	void Window_System::Scroll_Call_Back(GLFWwindow* Window, double X_Offset, double Y_Offset) {
+		const auto& App{ static_cast<Window_System*>(glfwGetWindowUserPointer(Window)) };
+		if (nullptr == App)
+			LOG_ERROR("App is nullptr");
+
+		App->On_Scroll(X_Offset, Y_Offset);
+	}
+
+	void Window_System::Drop_Call_Back(GLFWwindow* Window, int Count, const char** Paths) {
+		const auto& App{ static_cast<Window_System*>(glfwGetWindowUserPointer(Window)) };
+		if (nullptr == App)
+			LOG_ERROR("App is nullptr");
+
+		App->On_Drop(Count, Paths);
+	}
+
+	void Window_System::Window_Resize_Call_Back(GLFWwindow* Window, int Width, int Height) {
+		const auto& App{ static_cast<Window_System*>(glfwGetWindowUserPointer(Window)) };
+		if (nullptr == App) {
+			LOG_ERROR("App is nullptr");
+			throw std::runtime_error("App is nullptr");
+		}
+
+
+		App->m_Width = static_cast<uint32_t>(Width);
+		App->m_Height = static_cast<uint32_t>(Height);
+	}
+
+	void Window_System::Window_Close_Call_Back(GLFWwindow* Window) {
+		glfwSetWindowShouldClose(Window, GLFW_TRUE);
+	}
+
+} // namespace NameSpace_Window
